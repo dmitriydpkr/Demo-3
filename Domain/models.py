557abf041a4b78768d0478481f058e21623068af
ukrainer <dmitriydpkr@gmail.com>
2019-05-06 17:41:03 -0400

    models moved to domain folder

---
+++ Domain/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import *
+
+metadata = MetaData()
+
+contract = Table('contract', metadata,
+                 Column('id', Integer, primary_key=True, nullable=False),
+                 Column('title', VARCHAR(50), nullable=False),
+                 Column('amount', Float, nullable=False),
+                 Column('start_date', TIMESTAMP, nullable=False),
+                 Column('finish_date', TIMESTAMP, nullable=False))
+
+payment = Table('payment', metadata,
+                Column('id', Integer, primary_key=True, nullable=False),
+                Column('contributor', VARCHAR(50), nullable=False),
+                Column('amount', Float, nullable=False),
+                Column('date', DateTime, nullable=False),
+                Column('period', Integer, nullable=False),
+                Column('contract_id', None, ForeignKey('contract.id')))
+
+
+

1e2b306e9d72bfa6978ba3249aab4063f27055d0
ukrainer <dmitriydpkr@gmail.com>
2019-05-06 17:37:05 -0400

    functions are divided

---
+++ Domain/contracts.py
@@ -0,0 +1,24 @@
+import requests
+from database import *
+from Domain.models import payment
+
+contract_ip = "http://0.0.0.0:6000/contract/"
+
+
+async def send_request_contracts(contract_id):
+    try:
+        r = requests.get(contract_ip + str(contract_id))
+        return r.status_code
+    except requests.exceptions.RequestException as err:
+        print(err)
+
+
+async def get_contract(contract_number):
+
+    engine = await connect_db()
+    raw_data = []
+    async with engine.acquire() as conn:
+        async for row in conn.execute(payment.select().where(payment.c.contract_id == contract_number)):
+            raw_data.append(row)
+    return raw_data
+
--- Domain/payments.py
+++ Domain/payments.py
@@ -1,8 +1,5 @@
-import requests
 from database import *
-from models import payment
-import datetime
-contract_ip = "http://0.0.0.0:6000/contract/"
+from Domain.models import payment
 
 
 async def get_all_payments():
@@ -28,26 +25,7 @@ async def get_payment_period(start, finish):
     raw_data = []
     async with engine.acquire() as conn:
         query = payment.select().where(payment.c.period > start).where(payment.c.period < finish)
-        print(query)
         async for row in conn.execute(query):
-            raw_data.append(row)
-    return raw_data
-
-
-async def send_request_contracts(contract_id):
-    try:
-        r = requests.get(contract_ip + str(contract_id))
-        return r.status_code
-    except requests.exceptions.RequestException as err:
-        print(err)
-
-
-async def get_contract(contract_number):
-
-    engine = await connect_db()
-    raw_data = []
-    async with engine.acquire() as conn:
-        async for row in conn.execute(payment.select().where(payment.c.contract_id == contract_number)):
             raw_data.append(row)
     return raw_data
 
